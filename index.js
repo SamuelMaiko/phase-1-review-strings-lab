e64e9dbc5e8f5dbaed8485104bc10651ddc635df
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-13 19:40:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser="Samuel Maiko";
+const welcomeMessage="Welcome to Flatbook, "+currentUser+"!";
+const excitedWelcomeMessage="WELCOME TO FLATBOOK, "+currentUser.toUpperCase()+"!";
+const updatedName=currentUser.slice(0,1);
+const shortGreeting="Welcome, "+updatedName+"!";
